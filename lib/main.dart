837d86aba409b5fbf7dbbfe549f6078ce9254f26
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-06-29 22:30:00 -0300

    Scrollview

--- lib/main.dart
+++ lib/main.dart
@@ -29,48 +29,56 @@ class _HomeState extends State<Home> {
           ],
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(Icons.person_pin, size: 120, color: Colors.redAccent),
-          TextField (
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: "Your weight (kg)",
-              labelStyle: TextStyle(
-                  color: Colors.redAccent),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.person_pin, size: 120, color: Colors.redAccent),
+            TextField (
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Your weight (kg)",
+                labelStyle: TextStyle(
+                    color: Colors.redAccent),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 25,
+              ),
             ),
-            textAlign: TextAlign.center,
-            style: TextStyle(
+            TextField (
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Your Height",
+                labelStyle: TextStyle(
+                    color: Colors.redAccent),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 25,
+              ),
+            ),
+            Container(
+              height: 50.0,
+            ),
+            RaisedButton(
+              onPressed: () {},
+              child: Text(
+                "Calcular", style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
               color: Colors.red,
-              fontSize: 25,
             ),
-          ),
-          TextField (
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: "Your Height",
-              labelStyle: TextStyle(
-                  color: Colors.redAccent),
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.red,
-              fontSize: 25,
-            ),
-          ),
-          Container(
-            height: 50.0,
-          ),
-          RaisedButton(
-            onPressed: () {},
-            child: Text(
-              "Calcular", style: TextStyle(color: Colors.white, fontSize: 20),
-            ),
-            color: Colors.red,
-          ),
-        ],
-      ),
+            Text("Info",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 25,
+              ),)
+          ],
+        ),
+      )
     );
   }
 }
