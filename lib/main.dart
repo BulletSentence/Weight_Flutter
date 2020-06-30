0454b9fc64e62dc049a8bb8b1c013d3b921fe74e
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-06-30 10:02:02 -0300

    format

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main (){
+void main() {
   runApp(MaterialApp(
     home: Home(),
   ));
@@ -16,9 +16,8 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Weight Manager"),
+        appBar: AppBar(
+          title: Text("Weight Manager"),
           centerTitle: true,
           backgroundColor: Colors.redAccent,
           actions: <Widget>[
@@ -27,59 +26,58 @@ class _HomeState extends State<Home> {
               onPressed: () {},
             ),
           ],
-      ),
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Icon(Icons.person_pin, size: 120, color: Colors.redAccent),
-            TextField (
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Your weight (kg)",
-                labelStyle: TextStyle(
-                    color: Colors.redAccent),
-              ),
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 25,
-              ),
-            ),
-            TextField (
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Your Height",
-                labelStyle: TextStyle(
-                    color: Colors.redAccent),
-              ),
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 25,
-              ),
-            ),
-            Container(
-              height: 50.0,
-            ),
-            RaisedButton(
-              onPressed: () {},
-              child: Text(
-                "Calcular", style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-              color: Colors.red,
-            ),
-            Text("Info",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 25,
-              ),)
-          ],
         ),
-      )
-    );
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person_pin, size: 120, color: Colors.redAccent),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Your weight (kg)",
+                  labelStyle: TextStyle(color: Colors.redAccent),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 25,
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Your Height",
+                  labelStyle: TextStyle(color: Colors.redAccent),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 25,
+                ),
+              ),
+              Container(
+                height: 50.0,
+              ),
+              RaisedButton(
+                onPressed: () {},
+                child: Text(
+                  "Calcular",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+                color: Colors.red,
+              ),
+              Text(
+                "Info",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 25,
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
-
