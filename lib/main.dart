40401b06e3bbf94dfd0c7c670061dfaa6e272bb7
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-06-29 22:02:36 -0300

    Textfields and icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main (){
@@ -26,6 +27,49 @@ class _HomeState extends State<Home> {
               onPressed: () {},
             ),
           ],
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Icon(Icons.person_pin, size: 120, color: Colors.redAccent),
+          TextField (
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Your weight (kg)",
+              labelStyle: TextStyle(
+                  color: Colors.redAccent),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 25,
+            ),
+          ),
+          TextField (
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Your Height",
+              labelStyle: TextStyle(
+                  color: Colors.redAccent),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.red,
+              fontSize: 25,
+            ),
+          ),
+          Container(
+            height: 50.0,
+          ),
+          RaisedButton(
+            onPressed: () {},
+            child: Text(
+              "Calcular", style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            color: Colors.red,
+          ),
+        ],
       ),
     );
   }
